b6252a483486a9996e58c891da9753b7fcc72ca0
luojinxu <382967255@qq.com>
2023-07-06 19:33:40 +0800

    feat: adjust vite config.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,38 +5,15 @@ export default defineConfig({
     siteTitle: "锦衣",
     nav: [{ 
       text: 'ChatGPT', 
-      items: [
-        { 
-          text: 'ChatGpt账号注册', 
-          link: '/chatgpt/what-is-chatgpt' 
-        },
-        { 
-          text: 'ChatGpt私有化部署', 
-          link: '...' 
-        }
-      ]
+      link: "/chatgpt/what-is-chatgpt"
     }],
     sidebar: {
-      '/guide/': [
-        {
-          text: '介绍',
-          items: [
-            { text: '什么是 VitePress', link: '/guide/what-is-vitepress' },
-            { text: '快速开始', link: '/guide/getting-started' },
-            { text: '配置', link: '/guide/configuration' },
-            { text: '发布', link: '/guide/deploying' }
-          ],
-          collapsible: true
-        }
-      ],
       "/chatgpt/": [
         {
           text: '教程',
           items: [
-            { text: '什么是 ChatGpt', link: '/guide/what-is-vitepress' },
-            { text: '账号注册', link: '/guide/getting-started' },
-            { text: '配置', link: '/guide/configuration' },
-            { text: '发布', link: '/guide/deploying' }
+            { text: '什么是 ChatGpt', link: '/chatgpt/what-is-chatgpt' },
+            { text: '账号注册', link: '/chatgpt/register' },
           ],
           collapsible: true
         }
