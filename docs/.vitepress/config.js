6a41419884430a3a364d1462dc0ab6324fbf508e
luojinxu <382967255@qq.com>
2023-07-28 14:28:15 +0800

    Update config.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
   themeConfig: {
-    siteTitle: "锦衣",
-    nav: [{ 
+    logo: "/logo.svg",
+    siteTitle: "沧浪之巅",
+    nav: [{
       text: 'ChatGPT', 
       link: "/chatgpt/what-is-chatgpt"
     }],
@@ -13,7 +14,8 @@ export default defineConfig({
           text: '教程',
           items: [
             { text: '什么是 ChatGpt', link: '/chatgpt/what-is-chatgpt' },
-            { text: '账号注册', link: '/chatgpt/register' },
+            { text: '免费ChatGPT账号注册', link: '/chatgpt/register' },
+            { text: '升级ChatGPT Plus', link: '/chatgpt/plus' },
           ],
           collapsible: true
         }
