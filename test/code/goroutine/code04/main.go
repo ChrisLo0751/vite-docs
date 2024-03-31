baf8e396a206f5e18a909b24ed327794627f193e
luojinxu <382967255@qq.com>
2024-03-31 20:29:25 +0800

    chore: adjust file struct.

--- test/code/goroutine/code04/main.go
+++ test/code/goroutine/code04/main.go
@@ -10,7 +10,7 @@ import (
 var quit = make(chan int)
 
 func loop() {
-	for i:=0; i< 100; i++	 {
+	for i := 0; i < 100; i++ {
 		runtime.Gosched()
 		fmt.Println(i)
 	}
@@ -23,7 +23,7 @@ func main() {
 	go loop()
 	go loop()
 
-	for i:=0; i<2; i++ {
+	for i := 0; i < 2; i++ {
 		<-quit
 	}
 }
---
+++ test/code/goroutine/code05/nochce/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func consumer(ch chan int) {
+	defer wg.Done()
+	data := <-ch
+	fmt.Println("consumer: ", data)
+}
+
+func main() {
+	var ch1 = make(chan int, 1)
+
+	ch1 <- 1
+
+	wg.Add(1)
+	go consumer(ch1)
+	wg.Wait()
+
+	fmt.Println("Hello chanel")
+}
+
+// 在主协程中，chanel ch1 是一个无缓冲的channel，所以在第 10 行代码中，向 ch1 中写入数据会阻塞，导致主协程无法继续执行，从而导致 panic。
