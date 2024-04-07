c24a7a8129c96754e6ae4faf3d3e4052f3e93448
luojinxu <382967255@qq.com>
2024-04-07 18:30:49 +0800

    perf: adjust docs.

--- test/code/goroutine/code05/nochce/main.go
+++ test/code/goroutine/code05/nochce/main.go
@@ -25,4 +25,4 @@ func main() {
 	fmt.Println("Hello channel")
 }
 
-// 在主协程中，chanel ch1 是一个无缓冲的channel，所以在第 10 行代码中，向 ch1 中写入数据会阻塞，导致主协程无法继续执行，从而导致 panic。
+// 在主协程中，channel ch1 是一个无缓冲的channel，所以在第 10 行代码中，向 ch1 中写入数据会阻塞，导致主协程无法继续执行，从而导致 panic。
